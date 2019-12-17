528ac96ee8d911c266daed33b3729457a35103b8
brunoccy <brunochenchihying@gmail.com>
2019-12-17 09:53:34 -0300

    17/12 09:53

---
+++ testeSoma/test/testeSooma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mariana
+ */
+public class testeSomaJunit {
+    
+    public testeSomaJunit() {
+    }
+    
+}
