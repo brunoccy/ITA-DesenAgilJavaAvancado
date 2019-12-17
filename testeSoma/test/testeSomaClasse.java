46259fa3f126f27b65c279a598fcb8e1a0681a40
brunoccy <brunochenchihying@gmail.com>
2019-12-17 10:02:31 -0300

    17/12 10:02

---
+++ testeSoma/test/testeSomaClasse.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Mariana
+ */
+public class testeSomaClasse {
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @Before
+  public void setUp() {
+    driver = new ChromeDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void testesoma() {
+    driver.get("http://localhost:8080/TreinoTeste/");
+    driver.manage().window().setSize(new Dimension(1366, 728));
+    driver.findElement(By.name("p1")).click();
+    driver.findElement(By.name("p1")).sendKeys("5");
+    driver.findElement(By.name("p2")).click();
+    driver.findElement(By.name("p2")).sendKeys("11");
+    driver.findElement(By.name("calcular")).click();
+    driver.findElement(By.cssSelector("html")).click();
+    assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("O resultado 16"));
+  }
+}
